ae9165e6c146d01c59d5f1d0244dbb806d5decef
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2021-03-09 12:12:02 +0100

    Start & level Page as scenes

---
+++ Task-switching/Assets/Scripts/ResetBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+           
+    }
+
+    public void OnButtonPress()
+    {
+        // reset game advancement counter i.e. level counter
+        // prompt user if they really want to reset
+    }
+}
---
+++ Task-switching/Assets/Scripts/StartLevelBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevelBehaviour : MonoBehaviour
+{
+    int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnButtonPress()
+    {
+        // change scene to according level Page
+    }
+}
