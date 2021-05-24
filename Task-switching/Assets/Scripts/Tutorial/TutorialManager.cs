8b96bed408f7b492abca95f95ba061714d046007
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2021-05-24 13:49:33 +0200

    UPDATED Interface and ADDED first page to tutorial

---
+++ Task-switching/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    int page = 0;
+    int numberOfPages = 3;
+
+    void Start()
+    {
+        
+        refreshInterface();
+    }
+
+    public void onLeft()
+    {
+        Debug.Log("onLeft");
+        page = page - 1;
+        if(page < 0)
+        {
+            page = 0;
+        }
+        refreshInterface();
+    }
+
+    public void onRight()
+    {
+        Debug.Log("onRight");
+        page = page + 1;
+        if(page >= numberOfPages)
+        {
+            page = numberOfPages - 1;
+        }
+        refreshInterface();
+    }
+
+    public void refreshInterface()
+    {
+        Debug.Log("page:" + page);
+        if (page == 0)
+        {
+            GameObject.Find("LeftTutorial").GetComponent<Button>().interactable = false;
+            GameObject.Find("RightTutorial").GetComponent<Button>().interactable = true;
+        }
+        else if(page == numberOfPages - 1)
+        {
+            GameObject.Find("LeftTutorial").GetComponent<Button>().interactable = true;
+            GameObject.Find("RightTutorial").GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            GameObject.Find("LeftTutorial").GetComponent<Button>().interactable = true;
+            GameObject.Find("RightTutorial").GetComponent<Button>().interactable = true;
+        }
+    }
+}
